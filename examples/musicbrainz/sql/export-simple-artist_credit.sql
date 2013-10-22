ccaf4586d89c1d6d5f36bf7b8692eac5b43d4167
Paul Tremberth <paul.tremberth@gmail.com>
2013-10-22 14:53:52 +0200

    Remove old MusicBrainz example

--- examples/musicbrainz/sql/export-simple-artist_credit.sql
+++
@@ -1,7 +0,0 @@
-COPY (
-    SELECT
-        ac.id AS pk,
-        an.name
-    FROM artist_credit ac
-    JOIN artist_name an ON ac.name=an.id
-) TO stdout CSV HEADER DELIMITER E'\t';
--- examples/musicbrainz/sql/export-simple-url.sql
+++
@@ -1,9 +0,0 @@
--- COPY (SELECT * FROM url) TO stdout CSV HEADER DELIMITER E'\t';
-COPY(
-    SELECT
-        u.id AS pk,
-        u.gid AS mbid,
-        u.url
-    FROM url u
-)
-TO stdout CSV HEADER DELIMITER E'\t';
